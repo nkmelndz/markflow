0ad28cd78ea8d2e67785639428dc69e171537f97
nkmelndz <nkmelndz@gmail.com>
2026-01-08 22:47:39 -0500

    diseño slides bordes y margen

--- src/modules/editor/components/PreviewPanel.tsx
+++ src/modules/editor/components/PreviewPanel.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useMarpRender } from '@/modules/editor/hooks/useMarpRender';
+import styles from './PreviewPanel.module.css';
+import { clsx } from 'clsx';
 
 interface PreviewPanelProps {
   content: string;
@@ -12,7 +14,7 @@ export const PreviewPanel = ({ content }: PreviewPanelProps) => {
     <div className="h-full flex-1 min-w-[300px] bg-[#121212] flex flex-col items-center p-8 overflow-y-auto relative">
         <style>{css}</style>
         <div 
-          className="marp-content w-full max-w-4xl shadow-2xl"
+          className={clsx("marp-content", styles.previewContainer)}
           dangerouslySetInnerHTML={{ __html: html }} 
         />
     </div>
