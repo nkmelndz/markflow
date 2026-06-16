d2f1ac75e9f068b059699a560db32eb5b7228b47
nkmelndz <nkmelndz@gmail.com>
2026-06-16 13:06:49 -0500

    fix(ui): increase minLeftWidth to prevent editor toolbar overflow

--- src/modules/editor/hooks/useResizable.ts
+++ src/modules/editor/hooks/useResizable.ts
@@ -8,7 +8,7 @@ interface UseResizableProps {
 
 export const useResizable = ({ 
   initialWidthVw = 50,
-  minLeftWidth = 420,
+  minLeftWidth = 600,
   minRightWidth = 420,
 }: UseResizableProps = {}) => {
   const [width, setWidth] = useState<number>(initialWidthVw);
